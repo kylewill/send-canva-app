29d0305f0cebc941a9bc57b6e9de5a6a758e3df5
Kyle Williams <kylewill@gmail.com>
2026-02-12 13:49:28 -0500

    Remove design_editor intent to only show app in Share menu

--- canva-app/src/index.tsx
+++ canva-app/src/index.tsx
@@ -1,12 +1,4 @@
 import { prepareContentPublisher } from "@canva/intents/content";
-import { prepareDesignEditor } from "@canva/intents/design";
 import contentPublisher from "./intents/content_publisher";
-
-prepareDesignEditor({
-  render: () => {
-    // Design Editor intent is required as a base.
-    // Our app's UI is handled by the Content Publisher intent.
-  },
-});
 
 prepareContentPublisher(contentPublisher);
